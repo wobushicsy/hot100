45b75f06ae3fa7eac455da4396b07574184bd643
wobushicsy <csy657703828@163.com>
2024-03-20 10:36:00 +0800

    3.20 t2

---
+++ 101isSymmetric.cpp
@@ -0,0 +1,46 @@
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+  bool isSymmetric(TreeNode* root) {
+    if (root == nullptr) {
+      return true;
+    }
+    return isSymmetric(root->left, root->right);
+  }
+
+private:
+  bool isSymmetric(TreeNode *l, TreeNode *r) {
+    if (l == nullptr && r == nullptr) {
+      return true;
+    }
+    if (l == nullptr || r == nullptr) {
+      return false;
+    }
+    if (l->val != r->val) {
+      return false;
+    }
+    if (!isSymmetric(l->left, r->right)) {
+      return false;
+    }
+    return isSymmetric(l->right, r->left);
+  }
+};
