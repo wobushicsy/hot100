f6945f81adfc386d985d0ccc058237a4aa86280a
wobushicsy <csy657703828@163.com>
2024-03-20 10:24:19 +0800

    3.20 t1

---
+++ 226invertTree.cpp
@@ -0,0 +1,33 @@
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+  TreeNode* invertTree(TreeNode* root) {
+    if (root == nullptr) {
+      return root;
+    }
+    auto l = root->left;
+    auto r = root->right;
+    root->left = invertTree(r);
+    root->right = invertTree(l);
+    return root; 
+  }
+};
